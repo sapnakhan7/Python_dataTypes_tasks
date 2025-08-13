d437d4876d27c1298827f8611da20c2d11848253
sapnakhan7 <sapnaafazal@gmail.com>
2025-08-13 15:54:44 +0500

    make chnges in for loop 1 to 35

--- for_loop.py
+++ for_loop.py
@@ -1,5 +1,6 @@
 sum = 0
 for i in range(1, 36): 
     sum += i
+    # print("Sum from 1 to 35 is:", sum) // yahan print kia to loop k andr print hua isliye sare iterations ayi neeche space de knew line main kia to wo just sum print hua 
 
-print("Sum from 1 to 35 is:", sum)
+print("Sum from 1 to 35 is:", sum) # just sum print hua
