d8a3bd90847097ebb53e07d81d46ad754edabccb
sapnakhan7 <sapnaafazal@gmail.com>
2025-08-21 14:55:57 +0500

    balance and passowrd using while loop

---
+++ while_balance.py
@@ -0,0 +1,4 @@
+balance=int (input("Enter your balance: "))
+while balance > 0 :
+    print("Balance =" , balance)
+    balance-=5
---
+++ while_password.py
@@ -0,0 +1,6 @@
+password=""
+while password != "secret":
+    password = input("Enter the password: ")
+    print("Incorrect password, try again.")
+    
+print("Access granted!")
