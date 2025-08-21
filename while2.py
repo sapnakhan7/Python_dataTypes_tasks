b9ee1ac17dd67d8b9b3baa2ea741ef890514cfc8
sapnakhan7 <sapnaafazal@gmail.com>
2025-08-21 14:32:28 +0500

    added while loop some tasks , done using for loop

---
+++ while2.py
@@ -0,0 +1,7 @@
+total = 0
+i = 2
+w
+while i <= 50:
+    total += i**2
+    print(f"Step {i}, Sum = {total}")
+    i += 2  
---
+++ while3.py
@@ -0,0 +1,7 @@
+total = 0
+i = 1
+
+while i <= 50:
+    total += i**2
+    print(f"Step {i}, Sum = {total}")
+    i += 1
